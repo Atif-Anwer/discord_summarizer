e323b06168b920dfda150ba18049a3b81b213683
Atif <atif.anwer@u-bourgogne.fr>
2024-07-29 05:11:01 +0200

    Working OpenAPI integration with CV assistant

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from typing import Final
 
-from discord import Client, Intents, Message
+import discord
+from discord import Client, Intents, Message, app_commands
+from discord.ext import commands
 from dotenv import load_dotenv
 
 from responses import get_response
@@ -9,13 +11,16 @@ from responses import get_response
 # STEP 0: LOAD OUR TOKEN FROM SOMEWHERE SAFE
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+# openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# STEP 1: BOT SETUP
-intents: Intents        = Intents.default()
+# BOT SETUP
+intents                 = Intents.default()
 intents.message_content = True
-client : Client         = Client(intents=intents)
+client                  = Client(intents=intents)
+# client                  = commands.Bot(command_prefix="/", intents=intents)
+tree                    = app_commands.CommandTree(client)
 
-# STEP 2: MESSAGE FUNCTIONALITY
+# MESSAGE FUNCTIONALITY
 async def send_message(message: Message, user_message: str) -> None:
         if not user_message:
                 print('(Message was empty because intents were not enabled.)')
@@ -32,13 +37,13 @@ async def send_message(message: Message, user_message: str) -> None:
                 print(e)
 
 
-# STEP 3: HANDLING THE STARTUP FOR OUR BOT
+# HANDLING THE STARTUP FOR OUR BOT
 @client.event
 async def on_ready() -> None:
         print(f'{client.user} is now running!')
 
 
-# STEP 4: HANDLING INCOMING MESSAGES
+# HANDLING INCOMING MESSAGES
 @client.event
 async def on_message(message: Message) -> None:
     if message.author == client.user:
@@ -50,6 +55,26 @@ async def on_message(message: Message) -> None:
 
     print(f'[{channel}] {username}: "{user_message}"')
     await send_message(message, user_message)
+
+# bot = commands.Bot(command_prefix="/", intents=intents)
+
+@tree.command(name='ping')
+async def ping(interactions: discord.Interaction):
+    await interactions.response.send_message('pong')
+
+@client.event
+async def sync(ctx: commands .Context):
+    await ctx.send( 'Syncing...')
+    await client.tree. sync()
+
+@tree.command(name='sync', description='Owner only')
+async def sync(interactions: discord.Interaction):
+      if interactions.user.id == os.getenv('OWNER_ID'):
+            await tree.sync()
+
+@client.event
+async def yourmom(ctx: commands .Context):
+        await ctx.send( 'is cool')
 
 
 # STEP 5: MAIN ENTRY POINT
--- responses.py
+++ responses.py
@@ -1,20 +1,81 @@
-from random import choice, randint
+import os
+import time
+from typing import Final
+
+from dotenv import load_dotenv
+from openai import OpenAI
 
 
-def get_response(user_input: str) -> str:
-    lowered: str = user_input.lower()
+def load_openai_client_asistant():
+    client       = OpenAI( api_key = os.getenv("OPENAI_API_KEY") )
+    assistant_id = client.beta.assistants.retrieve( str( os.getenv("ASSISTANT_ID")))
+    thread       = client.beta.threads.create()
 
-    if lowered == '':
-        return 'Well, you\'re awfully silent...'
-    elif 'hello' in lowered:
-        return 'Hello there!'
-    elif 'how are you' in lowered:
-        return 'Good, thanks!'
-    elif 'bye' in lowered:
-        return 'See you!'
-    elif 'roll dice' in lowered:
-        return f'You rolled: {randint(1, 6)}'
-    else:
-        return choice(['I do not understand...',
-                       'What are you talking about?',
-                       'Do you mind rephrasing that?'])
+    return client, assistant_id, thread
+
+# check in loop  if assistant ai parse our request
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id = thread.id,
+            run_id    = run.id,
+        )
+        time.sleep(0.5)
+    return run
+
+def get_response(user_prompt: str) -> str:
+
+    user_prompt: str = user_prompt.lower()
+
+    # # Hardcoded text replies for testing
+    # if lowered == '':
+    #     return 'Well, you\'re awfully silent...'
+    # elif 'hello' in lowered:
+    #     return 'Hello there!'
+    # elif 'how are you' in lowered:
+    #     return 'Good, thanks!'
+    # elif 'bye' in lowered:
+    #     return 'See you!'
+    # elif 'roll dice' in lowered:
+    #     return f'You rolled: {randint(1, 6)}'
+    # else:
+    #     return choice(['I do not understand...',
+    #                    'What are you talking about?',
+    #                    'Do you mind rephrasing that?'])
+    # -------------------------------
+
+    # Add a message to the thread
+    message = client.beta.threads.messages.create(
+        thread_id = assistant_thread.id,
+        role      = "user",
+        content   = user_prompt,
+    )
+
+    # Run the assistant
+    run = client.beta.threads.runs.create(
+        thread_id    = assistant_thread.id,
+        assistant_id = assistant_id.id,
+    )
+
+    # Retreive the assistant run on the prompt
+    run = wait_on_run(run, assistant_thread)
+
+    # Print out all the messages added after our last user message
+    messages = client.beta.threads.messages.list(
+        thread_id = assistant_thread.id,
+        order     = "asc",
+        after     = message.id
+    )
+
+    print (message.role + ":" + message.content[0].text.value)
+
+    return messages.data[0].content[0].text.value
+
+
+load_dotenv()
+OPENAI_TOKEN: Final[str]                = os.getenv("OPENAI_API_KEY")
+client, assistant_id, assistant_thread  = load_openai_client_asistant()
+print(assistant_id)
+
+# References:
+# https://youtu.be/5rcjGjgJNQc
